ffb8a58a3e56ec943553d3b37c34b00898fa3860
DamianoMiloncini <miloncinid@gmail.com>
2024-09-24 20:33:31 -0400

    Adding CSS

--- iot_dashboard.py
+++ iot_dashboard.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -8,27 +8,23 @@ LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Function to control LED
-def control_led(state):
-    if state == 'on':
-        GPIO.output(LED_PIN, GPIO.HIGH)
-    else:
-        GPIO.output(LED_PIN,GPIO.LOW)
-
 @app.route('/')
 def index():
     # Load the web interface
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
-@app.route('/led_control', methods=['POST'])
-def led_control():
-    data = request.json
-    led_state = data['led']
-    control_led(led_state)
-    return jsonify(success=True, state=led_state)
+@app.route('/turn_on', methods=['POST'])
+def turn_on():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    return "ON"
+
+@app.route('/turn_off', methods=['POST'])
+def turn_off():
+    GPIO.output(LED_PIN, GPIO.LOW)
+    return "OFF"
 
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=5500, debug=True)
-    except KeyboardInterrupt:
+    finally:
         GPIO.cleanup()
